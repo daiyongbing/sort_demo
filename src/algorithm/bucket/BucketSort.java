b8cb03b094d74c42e3446e07e118c337c7d201c2
daiyongbing <37990048+daiyongbing@users.noreply.github.com>
2019-06-03 11:50:45 +0800

    桶排序

--- src/algorithm/bucket/BucketSort.java
+++ src/algorithm/bucket/BucketSort.java
@@ -1,4 +1,65 @@
 package algorithm.bucket;
 
+import java.util.ArrayList;
+
+/**
+ * @Description:    桶排序
+ * 设置一个定量的数组当作空桶；
+ * 遍历输入数据，并且把数据一个一个放到对应的桶里去；
+ * 对每个不是空的桶进行排序；
+ * 从不是空的桶里把排好序的数据拼接起来。
+ * @Author:         daiyongbing
+ * @CreateDate:     2019/6/3
+ * @Version:        1.0
+ */
 public class BucketSort {
+    public static ArrayList<Integer> bucketSort(ArrayList<Integer> array, int bucketSize) {
+        if (array == null || array.size() < 2)
+            return array;
+        int max = array.get(0), min = array.get(0);
+        // 找到最大值最小值
+        for (int i = 0; i < array.size(); i++) {
+            if (array.get(i) > max)
+                max = array.get(i);
+            if (array.get(i) < min)
+                min = array.get(i);
+        }
+        int bucketCount = (max - min) / bucketSize + 1;
+        ArrayList<ArrayList<Integer>> bucketArr = new ArrayList<>(bucketCount);
+        ArrayList<Integer> resultArr = new ArrayList<>();
+        for (int i = 0; i < bucketCount; i++) {
+            bucketArr.add(new ArrayList<Integer>());
+        }
+        for (int i = 0; i < array.size(); i++) {
+            bucketArr.get((array.get(i) - min) / bucketSize).add(array.get(i));
+        }
+        for (int i = 0; i < bucketCount; i++) {
+            if (bucketSize == 1) { // 如果带排序数组中有重复数字时  感谢 @见风任然是风 朋友指出错误
+                for (int j = 0; j < bucketArr.get(i).size(); j++)
+                    resultArr.add(bucketArr.get(i).get(j));
+            } else {
+                if (bucketCount == 1)
+                    bucketSize--;
+                ArrayList<Integer> temp = bucketSort(bucketArr.get(i), bucketSize);
+                for (int j = 0; j < temp.size(); j++)
+                    resultArr.add(temp.get(j));
+            }
+        }
+        return resultArr;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        ArrayList<Integer> returnList = new ArrayList<>();
+        arrayList.add(45);
+        arrayList.add(52);
+        arrayList.add(534);
+        arrayList.add(434);
+        arrayList.add(93);
+        arrayList.add(73);
+        arrayList.add(189);
+        arrayList.add(80);
+        returnList = bucketSort(arrayList, 3);
+        System.out.println(returnList);
+    }
 }
